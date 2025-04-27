0bbb82bdcf626430e932fca2743894d0a950db37
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-04-27 17:00:17 -0400

    the correct version of the rgb and hex

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./style.css";
 
 export default function RandomColor() {
   const [typeofColor, setTypeOfColor] = useState("hex");
@@ -27,13 +28,13 @@ export default function RandomColor() {
     setColor(rgbColor);
   }
 
-  useEffect(() => {
+  function handleGenerateRandomColor() {
     if (typeofColor === "rgb") {
       handleCreateRandomRgbColor();
     } else {
       handleCreateRandomHexColor();
     }
-  }, [typeofColor]);
+  }
 
   return (
     <div
@@ -51,16 +52,10 @@ export default function RandomColor() {
     >
       <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
       <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
-      <button
-        onClick={
-          typeofColor === "hex"
-            ? handleCreateRandomHexColor
-            : handleCreateRandomRgbColor
-        }
-      >
-        Generate Random Color
-      </button>
-      <h2 style={{ color: "#fff" }}>{color}</h2> {/* Show the color code */}
+
+      <button onClick={handleGenerateRandomColor}>Generate Random Color</button>
+
+      <h2 style={{ color: "#fff" }}>{color}</h2>
     </div>
   );
 }

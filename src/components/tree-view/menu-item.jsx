59997597d30794b627efad3974a39ff65e724044
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-04 14:25:13 -0400

    the tree toggle is working correctly

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -4,16 +4,28 @@ import MenuList from "./menu-list";
 export default function MenuItem({ item }) {
   const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
+  function handleToggleChildren(getCurrentLabel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
+
   return (
     <li>
-      <div>
+      <div style={{ display: "flex", gap: "20px" }}>
         <p>{item.label}</p>
         {item && item.children && item.children.length > 0 ? (
-          <span>+</span>
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? "-" : "+"}
+          </span>
         ) : null}
       </div>
 
-      {item && item.children && item.children.length > 0 ? (
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>

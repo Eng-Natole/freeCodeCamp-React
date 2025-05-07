7a406408a579f0297781c63fb9c8a0124f69654a
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-07 10:51:57 -0400

    intialization and setting the hook is complete for the theme

---
+++ src/components/light-dark/index.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function LightDark() {
+  return (
+    <div className="light-dark-mode">
+      <div className="container">
+        <p>Hello world!</p>
+        <button>Change Theme</button>
+      </div>
+    </div>
+  );
+}
+export default LightDark;

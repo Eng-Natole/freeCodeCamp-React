795187578eea53ea0545f569e64bbda57eeae93f
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-07 11:00:02 -0400

    now the hook is working correctly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/accordian";
 import ImageSlider from "./components/Image-slider";
+import LightDark from "./components/light-dark";
 import LoadMore from "./components/Load-more";
 import QrCodeGenrator from "./components/qr-code-generator";
 import RandomColor from "./components/random-color";
@@ -11,7 +12,7 @@ import menus from "./components/tree-view/data";
 function App() {
   return (
     <div className="App">
-      <QrCodeGenrator />
+      <LightDark />
     </div>
   );
 }
--- src/components/light-dark/index.jsx
+++ src/components/light-dark/index.jsx
@@ -1,11 +1,17 @@
 import "./style.css";
+import useLocalStorage from "./useLocalStorage";
 
 function LightDark() {
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+  function handleToggle() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
   return (
-    <div className="light-dark-mode">
+    <div className="light-dark-mode" data-theme={theme}>
       <div className="container">
         <p>Hello world!</p>
-        <button>Change Theme</button>
+        <button onClick={handleToggle}>Change Theme</button>
       </div>
     </div>
   );

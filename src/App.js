926cf633f439c58a35e7aa8089ca48ab2ab912f8
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-07 09:59:44 -0400

    css and the index code are complete but not working lets fix it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Accordian from "./components/accordian";
 import ImageSlider from "./components/Image-slider";
 import LoadMore from "./components/Load-more";
+import QrCodeGenrator from "./components/qr-code-generator";
 import RandomColor from "./components/random-color";
 import Star from "./components/star-rating";
 import TreeView from "./components/tree-view";
@@ -10,7 +11,7 @@ import menus from "./components/tree-view/data";
 function App() {
   return (
     <div className="App">
-      <TreeView menus={menus} />
+      <QrCodeGenrator />
     </div>
   );
 }
--- src/components/qr-code-generator/index.jsx
+++ src/components/qr-code-generator/index.jsx
@@ -1,7 +1,32 @@
+import QRCode from "react-qr-code";
 import "./style.css";
+import { use, useState } from "react";
 
 function QrCodeGenrator() {
-  return <div></div>;
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleGenerate() {
+    setQrCode(input);
+  }
+  return (
+    <div>
+      <h1>Qr Code Generator</h1>
+      <div>
+        <input
+          type="text"
+          name="qr-code"
+          placeholder="Enter your value here"
+          onChange={(e) => setInput(e.target.value)}
+          disabled={input && input.trim() !== "" ? false : true}
+        />
+        <button onClick={handleGenerate}>Generate</button>
+      </div>
+      <div>
+        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+      </div>
+    </div>
+  );
 }
 
 export default QrCodeGenrator;

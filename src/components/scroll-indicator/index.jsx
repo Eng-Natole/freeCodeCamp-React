19a7ad57136ff12744dd3576285dc5a6488219d2
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-11 03:40:13 -0400

    the progress  tracker is complete only css left

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -5,36 +5,61 @@ function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
   async function fetchData(getUrl) {
     try {
       setLoading(true);
-
       const response = await fetch(getUrl);
       const data = await response.json();
       console.log(data);
-
       if (data && data.products && data.products.length > 0) {
         setData(data.products);
-        setLoading(false);
       }
     } catch (error) {
       console.log(error);
-      setErrorMessage(error.errorMessage);
+      setErrorMessage(error.message || "Something went wrong.");
+    } finally {
+      setLoading(false);
     }
+  }
+
+  function handleScrollPercentage() {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrolled = (scrollTop / scrollHeight) * 100;
+    setScrollPercentage(scrolled);
   }
 
   useEffect(() => {
     fetchData(url);
   }, [url]);
-  console.log(data, loading);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+    return () => {
+      window.removeEventListener("scroll", handleScrollPercentage);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>Custom Scroll Indicator</h1>
+      <div className="top-container">
+        <h1>Custom Scroll Indicator</h1>
+        <div className="scroll-progress-tracking-container">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+
       <div className="data-container">
         {data && data.length > 0
-          ? data.map((dataItem) => <p>{dataItem.title}</p>)
-          : null}
+          ? data.map((dataItem) => <p key={dataItem.id}>{dataItem.title}</p>)
+          : !loading && <p>No data found</p>}
       </div>
     </div>
   );

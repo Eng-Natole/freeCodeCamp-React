7a4d44235e053288b66de7daa5550394238a6b38
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-12 15:44:45 -0400

    for the tabs folder the index and tab-test is half way

--- src/components/tab/index.jsx
+++ src/components/tab/index.jsx
@@ -1,4 +1,22 @@
-function Tab() {
-  return <div></div>;
+import { useState } from "react";
+
+function Tab({ tabsContent, onChange }) {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+  return (
+    <div className="wrapper">
+      <div className="heading">
+        {tabsContent.map((tabItem) => (
+          <div key={tabItem.label}>
+            <span className="label">{tabItem.label}</span>
+          </div>
+        ))}
+      </div>
+      <div className="content">
+        {tabsContent[currentTabIndex] &&
+          tabsContent[currentTabIndex].tabsContent}
+      </div>
+    </div>
+  );
 }
 export default Tab;
---
+++ src/components/tab/tab-test.jsx
@@ -0,0 +1,27 @@
+import Tab from "./index";
+
+function RandomComponent() {
+  return <h1>some random content</h1>;
+}
+
+export default function TabTest() {
+  const tabs = [
+    {
+      label: "Tab 1",
+      content: <div>this is content for tab 1</div>,
+    },
+    {
+      label: "Tab 2",
+      content: <div>this is content for tab 2</div>,
+    },
+    {
+      label: "Tab 3",
+      content: <RandomComponent />,
+    },
+  ];
+
+  function handleChange(currentTabIndex) {
+    console.log(currentTabIndex);
+  }
+  return <Tab tabsContent={tabs} onChange={handleChange} />;
+}

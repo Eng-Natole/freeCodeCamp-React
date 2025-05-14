52548bfb7dce0dc94aa9441c298f3c1bf4bf67e3
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-14 14:48:44 -0400

    we have added some user data loading states into it

--- src/components/github-profile/gitprofile.jsx
+++ src/components/github-profile/gitprofile.jsx
@@ -3,12 +3,18 @@ import "./git.css";
 
 function GitProfileFinder() {
   const [userName, setUserName] = useState("eng-natole");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   async function fetchGithubUserData() {
+    setLoading(true);
     const res = await fetch(`https:api.github.com/users/${userName}`);
 
     const data = await res.json();
-    console.log(data);
+    if (data) {
+      setUserData(data);
+      setLoading(false);
+    }
   }
 
   function handleSubmit() {}
@@ -17,6 +23,9 @@ function GitProfileFinder() {
     fetchGithubUserData();
   }, []);
 
+  if (loading) {
+    return <h1>Loading data! pleae wait</h1>;
+  }
   return (
     <div className="github-profile-container">
       <div className="input-wrapper">
